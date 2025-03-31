7ea5b3ee7552fb6f950f2adf754652b5beb56457
Rolan Zapara <rolan@iMac-Rolan.local>
2025-03-31 17:38:51 +0300

    changed script to get PID

--- L4_script.sh
+++ L4_script.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-echo "Scripting is fun"
-
-#!/bin/csh 
-echo "This script uses csh as the interpreter"
-
-#!/bin/ksh
-echo "This script uses ksh as the interpreter"
-
-#!/bin/zsh
-echo "This script uses zsh as the interpreter"
+sleep 90
