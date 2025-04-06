ecef673d9207e4fb97621724097d8e04b8b6a4f7
Rolan Zapara <rolan@iMac-Rolan.local>
2025-04-07 00:03:45 +0300

    added variable to the bash script and got new info regarding variables

--- L4_script.sh
+++ L4_script.sh
@@ -1,2 +1,7 @@
+# #!/bin/bash
+# print "This is a bash script"
+
 #!/bin/bash
-print "This is a bash script"
+MY_SHELL="bash"
+echo "I am $MY_SHELL on my keyboard"
+echo "I am ${MY_SHELL}ing on my keyboard"
