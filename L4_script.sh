3e8200fbf32bbd25f2fc729f53a5c08444d577ee
Rolan Zapara <rolan@iMac-Rolan.local>
2025-04-07 12:25:14 +0300

    test commit with -a

--- L4_script.sh
+++ L4_script.sh
@@ -1,7 +1,18 @@
-# #!/bin/bash
+\# #!/bin/bash
 # print "This is a bash script"
 
-#!/bin/bash
-MY_SHELL="bash"
-echo "I am $MY_SHELL on my keyboard"
+# VARIABLES
+# #!/bin/bash
+# MY_SHELL="bash"
+# echo "I am $MY_SHELL on my keyboard"
 echo "I am ${MY_SHELL}ing on my keyboard"
+
+# Assign command output to a variable
+# #!/bin/bash
+# SERVER_NAME=$(hostname)
+# echo "You are running this script on a ${SERVER_NAME}"
+
+# TESTS
+# Syntax [ condition-to-test-for ]
+# Example [ -e /etc/passwd ]
+
