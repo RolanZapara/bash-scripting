c571823a59f01637eb80ea760c6d42b40c625927
Rolan Zapara <rolan@iMac-Rolan.local>
2025-04-07 12:44:05 +0300

    added changes to script L5 with ELIF LOGIC

--- L5_script.sh
+++ L5_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-MY_SHELL="bash"
+MY_SHELL="csh"
 
 if [ "$MY_SHELL" = "bash" ]
 then
 	echo "You seem to like the bash shell."
+elif [ "$MY_SHELL" = "csh" ]
+then
+	echo "You seem to like the csh shell"
+else
+	echo "You do not seem to like the bash OR CSH shells. "
 fi
